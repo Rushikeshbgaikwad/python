a3ba393e09ba61ef08d3c0c8d5108a60c2d22162
Rushikeshbgaikwad <102414469+Rushikeshbgaikwad@users.noreply.github.com>
2022-04-22 14:08:21 +0530

    Add files via upload

---
+++ Accessing_string_char_in_python.py
@@ -0,0 +1,11 @@
+str="programiz"
+print("str = "+str)
+# print("str[0] = "+str[0] )
+# print("str[1] = "+str[1] )
+# print("str[2] = "+str[2] )
+# print("str[3] = "+str[3] )
+# print("str[4] = "+str[4] )
+print("str = " + str[1:5])
+print("str = " + str[5:-2])
+
+
